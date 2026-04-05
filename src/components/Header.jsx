493d238d40aa7bcceebedf0c70291e13a17a6be8
SemeshkoDmitriy <ist23-d.semeshko@nubip.edu.ua>
2026-04-05 16:12:57 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="container header-container">
+        <h1 className="logo">Machine Service</h1>
+
+        <nav className="nav">
+          <a href="#">Головна</a>
+          <a href="#">Каталог</a>
+          <a href="#">Про нас</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
